101c9b1479947b03938a74e7950c188918203719
art-Mohal-ab <mohamedfayafarah@gmail.com>
2025-09-22 08:13:13 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -20,9 +20,113 @@ db.init_app(app)
 api = Api(app)
 
 
+
+
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [
+            restaurant.to_dict(
+                only=(
+                    'id',
+                    'name',
+                    'address'
+                )
+            )
+            for restaurant in restaurants
+        ]
+
+
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {'error': 'Restaurant not found'}, 404
+        
+        return restaurant.to_dict(
+            only=(
+                'id',
+                'name',
+                'address',
+                'restaurant_pizzas.id',
+                'restaurant_pizzas.price',
+                'restaurant_pizzas.pizza_id',
+                'restaurant_pizzas.restaurant_id',
+                'restaurant_pizzas.pizza.id',
+                'restaurant_pizzas.pizza.name',
+                'restaurant_pizzas.pizza.ingredients'
+            )
+        )
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {'error': 'Restaurant not found'}, 404
+        
+        db.session.delete(restaurant)
+        db.session.commit()
+        
+    
+        return '', 204
+
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [
+            pizza.to_dict(
+                only=(
+                    'id',
+                    'name',
+                    'ingredients'
+                )
+            )
+            for pizza in pizzas
+        ]
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            
+            return restaurant_pizza.to_dict(
+                only=(
+                    'id',
+                    'price',
+                    'pizza_id',
+                    'restaurant_id',
+                    'pizza.id',
+                    'pizza.name',
+                    'pizza.ingredients',
+                    'restaurant.id',
+                    'restaurant.name',
+                    'restaurant.address'
+                )
+            ), 201
+        
+        except (ValueError, Exception):
+            # Return validation errors on failure
+            return {'errors': ['validation errors']}, 400
+
+
+# Registering routes with the API
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 
 if __name__ == "__main__":
